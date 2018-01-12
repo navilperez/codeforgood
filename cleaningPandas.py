7d1af3db7411b77a42acaf3decc38ecd255faa9d
Romeo <romflores95@gmail.com>
2018-01-11 22:43:05 -0500

    cleaningPandas.py is now fully functional for finding incorrect birthdate formats.

--- cleaningPandas.py
+++ cleaningPandas.py
@@ -31,26 +31,31 @@ birth = 'Date of birth (use calendar to select or enter as MM/DD/YYYY) (BirthDat
 # regex expression
 pattern = '[0-9][0-9][0-9][0-9][-][0-9][0-9][-][0-9][0-9]'
 
+def is_21_yrs(date):
+    '''checks if a date is at least 21 years old'''
+    now = datetime.datetime.now() # current time
+    year,month,day = int(date[:4]),int(date[5:7]),int(date[8:])
+    birthdate = datetime.date(year, month, day)
+
+    # in case of a leap year!
+    try: birthdate = datetime.datetime(year+21, month, day)
+    except: birthdate = datetime.datetime(year+21,3,1)
+
+    return birthdate <= now
+
+print(' ')
+print('Invalid birthdates:')
+print(' ')
+print('ID : input')
+print(' ')
 # searches through one column. Pythonic way
 for ind, val in dataset[birth].iteritems():
     #include is_21_yrs fxn
-    if type(val) != str or not(bool(re.match(pattern,val))):
+    if type(val) != str or not(bool(re.match(pattern,val))) or not is_21_yrs(val):
         # replace with ID instead of ind. Maybe a hash table. Maybe run zip with IDs. Maybe write ind + 2 to indicate row #
-        print(ind,val)
-
-def is_21_yrs(date):
-    '''checks if a date is at least 21 years ago'''
-    now = datetime.datetime.now()
-    year,month,day = int(date[:4]),int(date[5:7]),int(date[8:])
-    #return year <= now.year-21 and month >= now.month and day >= now.day
-    #if year < now.year-21: return True
-    #elif year > now.year-21:
+        print(ind, ':', val)
+print(' ')
 
 # pandas to extract all incorrectly formatted items
 #.str.extract(pat, flags=0, expand=None)[source]
-
-
-
-
-
 
