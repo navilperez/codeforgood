cfe943022c06085d430457a3d57c82dfc0d4e45e
Romeo <romflores95@gmail.com>
2018-01-12 11:56:52 -0500

    cleaningPandas.py improved formatting.

--- cleaningPandas.py
+++ cleaningPandas.py
@@ -9,7 +9,7 @@ import pandas as pd
 import random as rnd
 import re
 
-#import current day
+# import datetime
 import datetime 
 
 # import machine learning
@@ -23,16 +23,22 @@ from sklearn.linear_model import SGDClassifier
 from sklearn.tree import DecisionTreeClassifier
 
 # need to decide on data location
-dataset = pd.read_csv('/Users/romeoflores/Documents/code-for-good-local/data/report_Section_I_-_Demographic_Information_f6911ac7_2018.01.09.csv')
+dataset = pd.read_csv('report_Section_I_-_Demographic_Information_f6911ac7_2018.01.09.csv')
+
+# remove column heading spaces
+cols = dataset.columns
+cols = cols.map(lambda x: x.replace(' ', '_') if isinstance(x, str) else x)
+dataset.columns = cols
+print(dataset.columns)
 
 # column of interest
-birth = 'Date of birth (use calendar to select or enter as MM/DD/YYYY) (BirthDate)'
+birth = 'Date_of_birth_(use_calendar_to_select_or_enter_as_MM/DD/YYYY)_(BirthDate)'
 
 # regex expression
 pattern = '[0-9][0-9][0-9][0-9][-][0-9][0-9][-][0-9][0-9]'
 
 def is_21_yrs(date):
-    '''checks if a date is at least 21 years old'''
+    '''helper: checks if a date is at least 21 years old'''
     now = datetime.datetime.now() # current time
     year,month,day = int(date[:4]),int(date[5:7]),int(date[8:])
     birthdate = datetime.date(year, month, day)
@@ -44,18 +50,26 @@ def is_21_yrs(date):
     return birthdate <= now
 
 print(' ')
-print('Invalid birthdates:')
+print('------------------------------------------------------------------------------------------')
+print('Errors in: '+birth)
 print(' ')
-print('ID : input')
+print('Owner ID : input')
 print(' ')
 # searches through one column. Pythonic way
+total_errors = 0
 for ind, val in dataset[birth].iteritems():
-    #include is_21_yrs fxn
     if type(val) != str or not(bool(re.match(pattern,val))) or not is_21_yrs(val):
         # replace with ID instead of ind. Maybe a hash table. Maybe run zip with IDs. Maybe write ind + 2 to indicate row #
-        print(ind, ':', val)
+        print(dataset.iloc[ind]['Owner_ID'], ':', val)
+        total_errors += 1
+print(' ')
+print('Total errors found: '+ str(total_errors))
 print(' ')
 
-# pandas to extract all incorrectly formatted items
-#.str.extract(pat, flags=0, expand=None)[source]
 
+# To-Do's:
+# read general file through command line
+# email about:
+# we wrote script to identify birthdate errors. How do you want us to report errors? where does data go?, where is it visualized?, what other errors have you found? What data analysis have you already run on data - trends and/or machine learning?
+# write extra exception for invalid months and days
+# write read-me for how to use file
