6e4683a67d4b0715ad9ab30fedda01eaac4393ef
Romeo <romflores95@gmail.com>
2018-01-12 14:20:28 -0500

    cleaningPandas.py is generalized and fully functional.

--- cleaningPandas.py
+++ cleaningPandas.py
@@ -4,7 +4,6 @@ Romeo 01/09/18
 '''
 # pylint: disable=invalid-name
 
-
 ########################### Imports ###############################
 
 # interaction with the terminal
@@ -31,6 +30,37 @@ from sklearn.linear_model import Perceptron
 from sklearn.linear_model import SGDClassifier
 from sklearn.tree import DecisionTreeClassifier
 
+########################### Conditions and Constants ###############################
+
+# maps column names to list of conditions that need to be checked.
+# Can outsource this by importing excel file
+columns_to_conditions = {
+
+    'Date_of_birth_(use_calendar_to_select_or_enter_as_MM/DD/YYYY)_(BirthDate)' :
+        ['is_string', 'matches_pattern', 'is_21_yrs']
+
+}
+
+# maps condition names to functions
+# args = [column_name, arg1, arg2, arg3,...]
+conditions_to_functions = {
+
+    'is_string': lambda args: isinstance(args[1],str),
+    'is_21_yrs': lambda args: is_21_yrs(args[1]),
+    'matches_pattern': lambda args: bool(re.match(Constants[args[0]]['pattern'], args[1]))
+
+}
+
+# connects columns and constants. Hmmm... dict of dicts?
+Constants = {
+
+    'Date_of_birth_(use_calendar_to_select_or_enter_as_MM/DD/YYYY)_(BirthDate)' :
+        {
+            'pattern':
+            '[0-9][0-9][0-9][0-9][-][0-9][0-9][-][0-9][0-9]'
+        }
+}
+
 ########################### Helper Functions ###############################
 
 def column_formatting():
@@ -56,48 +86,84 @@ def is_21_yrs(date):
 
     return birthdate <= now
 
-########################### Script ##############################
+def find_errors(column_names):
+    '''iterates through every entry value in every column and prints errors'''
+    total_errors = 0
 
-# import file from terminal argument
-try:
-    df_survey = pd.read_csv(sys.argv[1]) # creates pandas dataframe
-except IndexError:
+    # iterate through each column
+    for column_name in column_names:
+        print_new_column(column_name)
+        column_errors = 0
+
+        # iterate through every entry in column
+        for ind, val in df_survey[birth].iteritems():
+
+            # iterate through each condition that has to be met
+            for condition in columns_to_conditions[column_name]:
+
+                # check each condition. Short-circuits if error is found
+                if not conditions_to_functions[condition]((column_name, val)):
+                    print(df_survey.iloc[ind]['Owner_ID'], ':', val)
+                    column_errors += 1
+                    total_errors += 1
+                    break
+
+        print(' ')
+        print('Column errors found: '+ str(column_errors))
+
+    print(' ')
+    print('Total errors found: '+ str(total_errors))
+    print(' ')
+
+def export_to_csv():
+    '''exports to csv file'''
+    pass
+
+def print_new_column(col):
+    '''prints messages to terminal'''
     print('')
-    print('ERROR! : ')
+    print('-----------------------------------------------------------------')
+    print('Errors in: '+ col)
+    print('')
+    print('Owner ID : cell number : input')
+    print('')
+    print('running...')
+    print('')
+
+def print_terminal_error():
+    '''prints if error in reading terminal input'''
+    print('')
+    print('ERROR!: ')
     print('No file found. Please select a file in the following format in terminal:')
     print('>python3 cleaningPandas.py filepath/filename.csv')
     print('')
-    print('Please note: Filepath can be relative or absolute.')
+    print('Please note: filepath can be relative or absolute.')
     print('')
+
+########################### Script ##############################
+
+# import file from terminal argument
+try: df_survey = pd.read_csv(sys.argv[1]) # creates pandas dataframe
+except IndexError:
+    print_terminal_error()
     sys.exit()
 
-column_formatting()
-
-# column of interest. can generalize by indexing dataframe and iterating
+# current column of interest. can generalize by indexing dataframe and iterating
 birth = 'Date_of_birth_(use_calendar_to_select_or_enter_as_MM/DD/YYYY)_(BirthDate)'
 
-# regex expression for dates https://regex101.com/
-pattern = '[0-9][0-9][0-9][0-9][-][0-9][0-9][-][0-9][0-9]'
+# update when we identify the mistakes of new columns
+column_names = [birth]
 
-print(' ')
-print('-----------------------------------------------------------------')
-print('Errors in: '+birth)
-print(' ')
-print('Owner ID : input')
-print(' ')
-# searches through one column. Pythonic way w/ help of pandas
-total_errors = 0
-for ind, val in df_survey[birth].iteritems():
-    if not isinstance(val, str) or not bool(re.match(pattern, val)) or not is_21_yrs(val):
-        print(df_survey.iloc[ind]['Owner_ID'], ':', val)
-        total_errors += 1
-print(' ')
-print('Total errors found: '+ str(total_errors))
-print(' ')
+#fix column formatting
+column_formatting()
+
+# find errors
+find_errors(column_names)
+
+# export errors to csv file
 
 # To-Do's:
-# generalize
 # write extra exception for invalid months and days
 # write read-me for how to use file
-# create dictionary that maps conditions to check for to functions such as is_21_yrs() and isintance(), patternMatch, etc.
-# create dictionary with entries for each column header and the necessary conditions and constants (such as required regex)
+# allow for conditions and constants to be read from csv file (kind of like a UI for the script!)
+# write key/rules for formatting of conditions and parameters
